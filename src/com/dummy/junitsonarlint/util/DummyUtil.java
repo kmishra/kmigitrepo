88de933ea97888ecbeb8cca1b4574a65c42a3954
Kundan Mishra <kmishra.tech@gmail.com>
2018-06-13 12:25:30 -0400

    SonarQuber reported issue fix.

--- src/com/dummy/junitsonarlint/util/DummyUtil.java
+++ src/com/dummy/junitsonarlint/util/DummyUtil.java
@@ -3,28 +3,15 @@ package com.dummy.junitsonarlint.util;
 public class DummyUtil {
 
 	public int getFinalAmount(int amount, int factor) {
-		Long amountForBroker = null;
 		Integer finalAmount = null;
-		 
-		try {
-
-		} catch (Exception e) {
-
-		}
-		
-		//TODO: Remove this as this is added to increase SQ issue count
-		Integer.valueOf(factor).intValue();
 
 		if (amount < 0) {
-			amountForBroker = 0L;
 			throw new IllegalArgumentException("Amount should not be less than 0");
 		} else if (amount > 999) {
-			amountForBroker = 0L;
 			throw new IllegalArgumentException("Amount should be less than 1000");
 		} else {			
 			finalAmount = amount / factor;
 		}
-		finalAmount.intValue();
 		
 		return finalAmount;
 
